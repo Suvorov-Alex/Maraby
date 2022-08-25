3aec3599918d25de3664687b92cb473e36cd184b
Alexander Suvorov <a.suvorov@fbs.com>
2022-08-25 22:20:32 +0200

    Implement Scrollable Column and Card Grid

--- app/src/main/java/com/suvorov/maraby/MainActivity.kt
+++ app/src/main/java/com/suvorov/maraby/MainActivity.kt
@@ -3,30 +3,15 @@ package com.suvorov.maraby
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
+import androidx.compose.foundation.ExperimentalFoundationApi
 
+
+@ExperimentalFoundationApi
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            ComposeGreetingScreen()
+            ComposeCardGrid()
         }
     }
-
-    @Composable
-    fun ComposeCardView() = CardView(
-        image = R.drawable.mountain,
-        text = getString(R.string.mountain_text),
-        withShadow = true,
-    )
-
-    @Composable
-    fun ComposeColorBox() = ColorBox(
-        Modifier.fillMaxSize()
-    )
-
-    @Composable
-    fun ComposeGreetingScreen() = GreetingScreen()
 }
