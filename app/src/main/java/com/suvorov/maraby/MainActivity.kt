dccda8e23ee1a90a5b8035c56dd27d5c8cb6dfc7
Alexander Suvorov <a.suvorov@fbs.com>
2022-08-25 19:31:04 +0200

    Work with state in compose

--- app/src/main/java/com/suvorov/maraby/MainActivity.kt
+++ app/src/main/java/com/suvorov/maraby/MainActivity.kt
@@ -3,16 +3,28 @@ package com.suvorov.maraby
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.appcompat.app.AppCompatActivity
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            CardView(
-                image = R.drawable.mountain,
-                text = getString(R.string.mountain_text),
-                withShadow = true,
-            )
+            InflateColorBox()
         }
     }
+
+    @Composable
+    fun InflateCardView() = CardView(
+        image = R.drawable.mountain,
+        text = getString(R.string.mountain_text),
+        withShadow = true,
+    )
+
+    @Composable
+    fun InflateColorBox() = ColorBox(
+        Modifier.fillMaxSize()
+    )
 }
